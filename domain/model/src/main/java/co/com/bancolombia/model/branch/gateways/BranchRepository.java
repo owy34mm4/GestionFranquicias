240afdf0eb8a7e544b1ca2ec704f188de00f5a61
owy34mm4 <y0rmun009+gh@gmail.com>
2026-07-03 14:53:34 -0500

    feat: add repository interfaces for the models

--- domain/model/src/main/java/co/com/bancolombia/model/branch/gateways/BranchRepository.java
+++ domain/model/src/main/java/co/com/bancolombia/model/branch/gateways/BranchRepository.java
@@ -1,4 +1,11 @@
 package co.com.bancolombia.model.branch.gateways;
 
+import co.com.bancolombia.model.branch.Branch;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface BranchRepository {
+    Mono<Branch> save(Branch branch);
+    Mono<Branch> findById(Long id);
+    Flux<Branch> findAll();
 }
--- domain/model/src/main/java/co/com/bancolombia/model/product/gateways/ProductRepository.java
+++ domain/model/src/main/java/co/com/bancolombia/model/product/gateways/ProductRepository.java
@@ -1,4 +1,11 @@
 package co.com.bancolombia.model.product.gateways;
 
+import co.com.bancolombia.model.product.Product;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 public interface ProductRepository {
+    Mono<Product> save(Product product);
+    Mono<Product> findById(Long id);
+    Flux<Product> findAll();
 }

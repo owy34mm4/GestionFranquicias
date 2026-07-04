29c670b31b9fb6984509c0f47d647b792bb63967
owy34mm4 <y0rmun009+gh@gmail.com>
2026-07-04 14:05:57 -0500

    feat: add create franchise use case

--- domain/usecase/src/main/java/co/com/gestorfranquicia/usecase/franchise/FranchiseUseCase.java
+++ domain/usecase/src/main/java/co/com/gestorfranquicia/usecase/franchise/FranchiseUseCase.java
@@ -1,6 +1,22 @@
 package co.com.gestorfranquicia.usecase.franchise;
 
+import co.com.gestorfranquicia.model.enums.TechnicalMessage;
+import co.com.gestorfranquicia.model.exception.BusinessException;
+import co.com.gestorfranquicia.model.franchise.Franchise;
+import co.com.gestorfranquicia.model.franchise.gateways.FranchiseRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
 @RequiredArgsConstructor
 public class FranchiseUseCase {
+
+    private final FranchiseRepository franchiseRepository;
+
+    public Mono<Franchise> create(String name) {
+        return franchiseRepository.existsByName(name)
+                .filter(exists -> !exists)
+                .switchIfEmpty(Mono.error(new BusinessException(TechnicalMessage.FRANCHISE_ALREADY_EXISTS)))
+                .then(Mono.fromCallable(() -> Franchise.create(name)))
+                .flatMap(franchiseRepository::save);
+    }
 }
